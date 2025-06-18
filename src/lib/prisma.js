81b6d4a250abcd3cd81fdfdd83882377a57edbfa
batug46 <gbatu4242@gmail.com>
2025-06-18 23:30:46 +0300

    Fix Prisma import issues

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,8 +2,8 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export default prisma; 
+export { prisma }; 
