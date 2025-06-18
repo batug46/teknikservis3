504a09446ec17b2bb0ff882a3d1e11a9a8132379
batug46 <gbatu4242@gmail.com>
2025-06-18 23:27:56 +0300

    Update more prisma import paths

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '../../../../lib/prisma'; // YOL DÜZELTİLDİ
+import prisma from '@/lib/prisma';
 
 export async function GET(request, { params }) {
   try {
